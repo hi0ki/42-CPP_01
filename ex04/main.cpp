a92e36e59e169f21306a4cde28473ee5d9cee42f
hi0ki <ansarimehdi518@gmail.com>
2025-01-14 09:36:57 +0100

    ex04 done

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
-
-//std::ifstream to read from a file
-//std::ofstream to write to a file
-
-
-
-
+#include <fstream>
 
 int main(int ac, char **av)
 {
@@ -15,9 +9,33 @@ int main(int ac, char **av)
 		std::cout << "too many parameters" << std::endl;
 	else 
 	{
-		std::string filename(av[1]);
-		std::string str1(av[2]);
-		std::string str2(av[3]);
-		std::cout << str1 << filename << str2 << std::endl;
+		std::string filename = av[1];
+		std::string file_replace = filename + ".replace";
+		std::string str1 = av[2];
+		std::string str2 = av[3];
+		std::string line;
+		int i = 0;
+
+		std::ifstream ifile(filename);
+		std::ofstream ofile(file_replace);
+		if (!ifile.is_open() || !ofile.is_open())
+		{
+			std::cout << "Error: Cannot open file" << std::endl;
+			return (1);
+		}
+		while (getline(ifile, line))
+		{
+			int i = 0;
+			while ((i = line.find(str1, i)) != -1)
+			{
+				line.erase(i, str1.length());
+				line.insert(i, str2);
+				i += str2.length();
+			}
+			ofile << line << std::endl;
+		}
+		ifile.close();
+		ofile.close();
 	}
+	return (0);
 }
