8c65077f59f2c5ba54ae1da75eb5a0ababd41d04
hi0ki <ansarimehdi518@gmail.com>
2025-01-15 04:10:03 +0100

    all done

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,16 +1,10 @@
 #include "Zombie.hpp"
 
-void ll()
-{
-	system("leaks program");
-}
-
 int main()
 {
-	atexit(ll);
 	int i = 0;
 	int nb_of_zombies = 5;
-	Zombie *objs = zombieHorde(nb_of_zombies, "mehdi");
+	Zombie *objs = zombieHorde(nb_of_zombies, "foo");
 	while (i < nb_of_zombies)
 	{
 		objs[i].announce();
